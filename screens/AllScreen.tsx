4b11dcb77ec53f842b44f7ac563379296ed7f368
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2021-02-20 21:06:34 +0700

    ➕ Added 'add' button to AllScreen

--- screens/AllScreen.tsx
+++ screens/AllScreen.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Text, View } from "../components/Themed";
 import AllToDoList from "../components/AllToDoList";
+import useColorScheme from "../hooks/useColorScheme";
 
 export default function AllScreen() {
   const a = [1, 2, 3];
   return (
     <View style={styles.container}>
+      <TouchableOpacity>
+        <MaterialIcons
+          name="add-box"
+          size={35}
+          color={useColorScheme() === "dark" ? "white" : "black"}
+        />
+      </TouchableOpacity>
       <AllToDoList />
     </View>
   );
@@ -18,7 +27,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 20,
+    paddingTop: 25,
   },
   title: {
     fontSize: 20,
