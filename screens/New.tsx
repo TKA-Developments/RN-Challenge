52d6ba9a92cd841b4f12ee46a7950db1f2aee249
prima maharyono <pmaharyono@gmail.com>
2021-03-29 04:46:44 +0700

    init

---
+++ screens/New.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+// import { Ionicons } from '@expo/vector-icons';
+
+export default function New(){
+        return (
+        <View>
+            <Text>halo</Text>
+        </View>
+    )
+}
+
