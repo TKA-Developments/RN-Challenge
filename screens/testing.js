90d5f506702ccd901d5bd3867e9e4d9452a33b57
Parakacu <parakacu@gmail.com>
2021-04-03 17:34:28 +0700

    testing

---
+++ screens/testing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const testing = () => {
+    return (
+        <View>
+            <Text> Test </Text>
+        </View>
+    )
+}
+
+export default testing
+
+const styles = StyleSheet.create({})
