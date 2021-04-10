abe9393079444921a3d970d9c163ccb5947da436
iqbal2906 <maniacjoe3@gmail.com>
2021-04-10 15:20:08 +0700

    validation at login for email format, password, and reenter password

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -2,8 +2,27 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Colors from "../constants/Colors";
 import Button from "../components/Button";
+import validator from "validator";
+
+const validateFields = (email, password) => {
+  const isValid = {
+    email: validator.isEmail(email),
+    password: validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    }),
+  };
+  return isValid;
+};
 
 import LabeledInput from "../components/LabeledInput";
+
+const createAccount = (email, password) => {};
+
+const login = (email, password) => {};
 
 export default () => {
   const [isCreateMode, setIsCreateMode] = useState(true);
@@ -76,7 +95,34 @@ export default () => {
       </View>
 
       <Button
-        onPress={() => {}}
+        onPress={() => {
+          const isValid = validateFields(emailField.text, passwordField.text);
+          let isAllValid = true;
+          if (!isValid.email) {
+            emailField.errorMessage = "Please enter a valid email";
+            setEmailField({ ...emailField });
+            isAllValid = false;
+          }
+
+          if (!isValid.password) {
+            passwordField.errorMessage =
+              "Password must be at least 8 long w/numbers, uppercase, lowercase, and symbol character";
+            setPasswordField({ ...passwordField });
+            isAllValid = false;
+          }
+
+          if (isCreateMode && passwordReentryField.text != passwordField.text) {
+            passwordReentryField.errorMessage = "Password do not match";
+            setPasswordReentryField({ ...passwordReentryField });
+            isAllValid = false;
+          }
+
+          if (isAllValid) {
+            isCreateMode
+              ? createAccount(emailField.text, passwordField.text)
+              : login(emailField.text, passwordField.text);
+          }
+        }}
         buttonStyle={{ backgroundColor: Colors.red }}
         text={isCreateMode ? "Create Account" : "Login"}
       />
