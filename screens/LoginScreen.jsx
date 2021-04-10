a6f3f1842b051e7dc221c1803c2b6ab08f816bc0
iqbal2906 <maniacjoe3@gmail.com>
2021-04-10 14:59:16 +0700

    complete basic UI login and register

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,14 +1,79 @@
-import React, { useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Colors from "../constants/Colors";
 import Button from "../components/Button";
 
+import LabeledInput from "../components/LabeledInput";
+
 export default () => {
   const [isCreateMode, setIsCreateMode] = useState(true);
+  const [emailField, setEmailField] = useState({
+    text: "",
+    errorMessage: "",
+  });
+  const [passwordField, setPasswordField] = useState({
+    text: "",
+    errorMessage: "",
+  });
+  const [passwordReentryField, setPasswordReentryField] = useState({
+    text: "",
+    errorMessage: "",
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>TodoApp</Text>
-      <View style={{ flex: 1 }}></View>
+      <View style={{ flex: 1 }}>
+        <LabeledInput
+          label="Email"
+          text={emailField.text}
+          onChangeText={(text) => {
+            setEmailField({ text });
+          }}
+          errorMessage={emailField.errorMessage}
+          labelStyle={styles.label}
+          autoCompleteType="email"
+        />
+        <LabeledInput
+          label="Password"
+          text={passwordField.text}
+          onChangeText={(text) => {
+            setPasswordField({ text });
+          }}
+          secureTextEntry={true}
+          errorMessage={passwordField.errorMessage}
+          labelStyle={styles.label}
+          autoCompleteType="password"
+        />
+        {isCreateMode && (
+          <LabeledInput
+            label="Re-enter password"
+            text={passwordReentryField.text}
+            onChangeText={(text) => {
+              setPasswordReentryField({ text });
+            }}
+            secureTextEntry={true}
+            errorMessage={passwordReentryField.errorMessage}
+            labelStyle={styles.label}
+          />
+        )}
+        <TouchableOpacity
+          onPress={() => {
+            setIsCreateMode(!isCreateMode);
+          }}
+        >
+          <Text
+            style={{
+              alignSelf: "center",
+              color: Colors.blue,
+              fontSize: 16,
+              margin: 4,
+            }}
+          >
+            {isCreateMode ? "Already have an account?" : "Create new account"}
+          </Text>
+        </TouchableOpacity>
+      </View>
 
       <Button
         onPress={() => {}}
