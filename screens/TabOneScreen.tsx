85f05bfe9cbfb4659bbb30b08ad57c3612965c94
bruhtus <hibruhtus@gmail.com>
2021-09-06 11:16:40 +0700

    Commented out unused tab one screen element

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
@@ -17,8 +14,8 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 20,
@@ -30,3 +27,12 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+// unused default component
+// import EditScreenInfo from '../components/EditScreenInfo';
+      // <View
+      //   style={styles.separator}
+      //   lightColor="#eee"
+      //   darkColor="rgba(255,255,255,0.1)"
+      // />
+      // <EditScreenInfo path="/screens/TabOneScreen.tsx" />
