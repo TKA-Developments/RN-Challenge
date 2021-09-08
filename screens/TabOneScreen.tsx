2bbd3cc43ab89f60eb41afd70bfb3ee830bbdd7d
bruhtus <hibruhtus@gmail.com>
2021-09-08 09:24:44 +0700

    Tab one: clean up unused default components
    To make `TabOneScreen.tsx` simpler and less line of code. If I need
    reference from default components, I can just look in the main branch.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import TaskItems from '../components/TaskItems';
@@ -20,21 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e8eaed',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
-
-// unused default component
-// import EditScreenInfo from '../components/EditScreenInfo';
-      // <View
-      //   style={styles.separator}
-      //   lightColor="#eee"
-      //   darkColor="rgba(255,255,255,0.1)"
-      // />
-     // <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-   // separator: {
-   //  marginVertical: 30,
-   //  height: 1,
-   //  width: '80%',
-  // },
