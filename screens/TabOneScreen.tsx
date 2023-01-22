2d855689f3953e90d1450b041c1dfb8023f30886
ASUS <joshuawenata@gmail.com>
2023-01-23 03:23:08 +0700

    read data from firebase

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,38 @@
+import { child, get, ref } from 'firebase/database';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, ScrollView, StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import { db } from '../configfirebase';
 
 export default function TabOneScreen() {
+  const dbref = ref(db);
+  const [data, setData] = useState<any[]>([]);
+  var array: any[] = [];
 
+  function fetchData() {
+    get(child(dbref, 'task/')).then((snapshot) => {
+      if(snapshot.exists()){
+        snapshot.forEach(childsnapshot => {
+          array = [...array,childsnapshot.val().task];
+          setData(array);
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Today Task</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {data.map((e)=>
+        <Text style={styles.item}>{e}</Text>
+      )}
     </View>
   );
 }
@@ -28,4 +52,7 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  item:{
+    margin: 10
+  }
 });
